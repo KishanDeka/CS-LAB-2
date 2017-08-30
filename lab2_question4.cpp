4b7617442ddd3e691779591228a280836356eea1
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-08-30 15:32:30 +0530

    Create lab2_question4.cpp

---
+++ lab2_question4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main ()
+{ float d;
+  cout << "enter number of days = ";
+  cin >> d;
+  cout << "conversion into years is = " << d/365<< endl;
+  cout << "conversion into weeks is = " << d/7 ;
+  return 0;
+}
