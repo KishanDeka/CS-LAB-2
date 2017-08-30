332d92fc99afa9417057aedb52d9c67d438c1134
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-08-30 14:59:40 +0530

    Update and rename q to lab2_question6.cpp

---
+++ lab2_question6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main ()
+{ float x;
+  cout<< "enter your number"<< endl;
+  cin >> x;
+  cout <<"squareroot of the number is " << sqrt(x);
+  return 0;
+}
