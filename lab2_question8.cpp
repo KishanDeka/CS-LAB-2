cbec8555346fc0ade98c1551428f6c51eced972c
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-08-30 15:45:34 +0530

    Create lab2_question8.cpp

---
+++ lab2_question8.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main ()
+{ float b, h;
+  cout << "enter the base and height of a triangle respectively"<<endl;
+  cin >> b >> h;
+  cout << "the area of the triangle is = " << (h*b)/2;
+  return 0;
+}
