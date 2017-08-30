a39ec7b9c915a95dd23b3150e2e6761082bed54f
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-08-30 14:53:34 +0530

    Create lab2_question5.cpp

---
+++ lab2_question5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main ()
+{ int x, y;
+  cout<< "enter your base"<< endl;
+ cin >> x;
+  cout << "enter your exponent" << endl;
+  cin >> y;
+  cout <<"your answer is " << pow(x,y);
+  return 0;
+}
