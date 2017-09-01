2e562e258dba2302860ac178718d98e4e1ac5251
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-09-01 18:32:40 +0530

    Update lab2_question1.cpp

--- lab2_question1.cpp
+++ lab2_question1.cpp
@@ -7,7 +7,7 @@ int main ()
 { float x;
   cout << "enter the length in centimeter = ";
   cin >> x;
-  cout << "your length in meter = " << x/100;
+  cout << "your length in meter = " << x/100<<endl;
   cout << "your length in kilometer = " << x/100000;
   return 0;
 }
