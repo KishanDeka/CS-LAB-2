11ca5641f56b68a3c539249253fece1368b39fa0
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-09-01 18:09:15 +0530

    Create lab2_question11.cpp

---
+++ lab2_question11.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+ float p, r, t;
+ cout << "enter P,T & R to calculate your simple interest" << endl;
+ cin >> p>>r>>t;
+ cout << "the simple interest is = "<< (p*r*t)/100;
+
+
+
+    return 0;
+}
